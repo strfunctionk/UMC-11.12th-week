39f95167ceff87745e81a998acb964870ac60f47
strfunctionk <84651690+strfunctionk@users.noreply.github.com>
2025-01-03 00:39:01 +0900

    Bigint

--- src/index.ts
+++ src/index.ts
@@ -31,16 +31,7 @@ import {
 } from "./auth.config.js";
 import { prisma } from "./db.config.js";
 
-const obj: { value: BigInt } = {
-    value: BigInt(1234567890123456789)
-};
-
-const jsonString = JSON.stringify(obj, (key, value) => {
-    if (typeof value === 'bigint') {
-        return value.toString();
-    }
-    return value;
-});
+BigInt.prototype.toJSON = function () { return this.toString() };
 
 dotenv.config();
 
